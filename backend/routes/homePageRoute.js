c33da3e66945bb13ac4ddf81faef01796bed1eef
ijjaasss <ijasmuthu04@gmail.com>
2025-08-03 17:38:43 +0530

    update some place

--- backend/routes/homePageRoute.js
+++ backend/routes/homePageRoute.js
@@ -3,11 +3,12 @@ import express from 'express';
 
 import upload from '../middlewares/upload.js';
 import { getBackgroundImage, uploadBackgroundImage } from '../controllers/homePageController.js';
+import { protect } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
 
-router.post('/upload', upload.single('image'), uploadBackgroundImage);
+router.post('/upload', upload.single('image'), protect ,uploadBackgroundImage);
 router.get('/', getBackgroundImage);
 
 export default router;
--- backend/routes/recentImageRoutes.js
+++ backend/routes/recentImageRoutes.js
@@ -2,10 +2,11 @@
 import express from 'express';
 import upload from '../middlewares/upload.js';
 import { deleteRecentImage, getRecentImages, uploadRecentImage } from '../controllers/recentImageController.js';
+import { protect } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
-router.post('/upload', upload.single('image'), uploadRecentImage);
+router.post('/upload', upload.single('image'), protect,uploadRecentImage);
 router.get('/', getRecentImages);
-router.delete('/delete/:id', deleteRecentImage);
+router.delete('/delete/:id', protect,deleteRecentImage);
 export default router;
