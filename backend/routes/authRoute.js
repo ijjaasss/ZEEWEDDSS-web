162b0696f1c9dc2216469ca2c6b494d2bc0fcabd
ijjaasss <ijasmuthu04@gmail.com>
2025-08-04 14:22:39 +0530

    delete(register):register route delete

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,10 +1,10 @@
 import express from 'express'
-import { getCurrentUser, loginUser, logoutUser, registerUser } from '../controllers/authController.js';
+import { getCurrentUser, loginUser, logoutUser } from '../controllers/authController.js';
 import { protect } from '../middlewares/authMiddleware.js';
 const router = express.Router();
 
 
-// router.post('/register', registerUser);
+
 router.post('/login', loginUser);
 router.post('/logout', logoutUser);
 router.get('/me', protect, getCurrentUser);
